8ee7a1bd5249cf438602e00f9693b7d1b2945409
feriadi <feriadiputra6@gmail.com>
2025-11-20 08:46:53 +0700

    test

--- worker/Program.cs
+++ worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=lks-rds.cvnb1e2wtrmb.us-east-1.rds.amazonaws.com:5432;Username=postgres;Password=LKSNCC2024;");
+                var pgsql = OpenDbConnection("Server=lks-rds.cvnb1e2wtrmb.us-east-1.rds.amazonaws.com;Username=postgres;Password=LKSNCC2024;");
                 var redisConn = OpenRedisConnection("lks-redis.lfc5mu.ng.0001.use1.cache.amazonaws.com");
                 var redis = redisConn.GetDatabase();
 
@@ -46,7 +46,7 @@ namespace Worker
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
                             Console.WriteLine("Reconnecting DB");
-                            pgsql = OpenDbConnection("Server=lks-rds.cvnb1e2wtrmb.us-east-1.rds.amazonaws.com:5432;Username=postgres;Password=LKSNCC2024;");
+                            pgsql = OpenDbConnection("Server=lks-rds.cvnb1e2wtrmb.us-east-1.rds.amazonaws.com;Username=postgres;Password=LKSNCC2024;");
                         }
                         else
                         { // Normal +1 vote requested
